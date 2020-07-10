8dd37c258a51fbbdc829892595565a326021c04e
drinkoder <drinkoder@gmail.com>
2020-07-10 03:43:49 +0100

    added cors

--- main.ts
+++ main.ts
@@ -3,9 +3,12 @@ import {
   Router,
   RouterContext,
 } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
 import * as flags from "https://deno.land/std/flags/mod.ts";
 import { fetchAllData } from "./routes.ts";
 
+const uri = "https://api-moroccan-map.herokuapp.com";
 const { args } = Deno;
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(args).port;
@@ -13,18 +16,17 @@ const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
 const app = new Application();
 const router = new Router();
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
 router
   .get("/", (ctx: RouterContext) => {
-    ctx.response.body = `
-        Welcome to the API Moroccan Map
-            /data   :   get all map json (regions/sub_regions/divisions/sub_divisions) 
- `;
+    ctx.response.body = `Welcome to the API Moroccan Map
+        ${uri}/data   :   get all map json (regions/sub_regions/divisions/sub_divisions) 
+    `;
   })
   .get("/data", fetchAllData);
+
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 console.log(`Server is running on port ${port}`);
 
